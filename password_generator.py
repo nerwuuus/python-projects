76c87f77f59b5924d8c039dd1e70a39037868ca0
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2025-09-27 20:39:53 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -9,7 +9,6 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# easy version (order: letters, symbols and numbers)
 password = ""
 
 for char in range(0, nr_letters):
