735b58f46a6f8789dc35ab31d3ab11d1ef6571d7
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2025-11-17 17:32:29 +0100

    Update web_scraping_using_pandas.py

--- beginner_web_scraping_project/web_scraping_using_pandas.py
+++ beginner_web_scraping_project/web_scraping_using_pandas.py
@@ -1,21 +1,39 @@
-# ============================================================================
-# This script downloads all .csv files containing data for 
-# every Premier League match during the 2021/2022 season
-# ============================================================================
+# ====================================================================================
+# This script downloads all .csv files containing data for every Premier League match:
+#   - during the 2021/2022 season
+#   - or from 2015 to 2022 seasons
+# ====================================================================================
 import pandas as pd
 
-# URL structure and parameters
-# https://www.football-data.co.uk/mmz4281/2122/E0.csv
+# URL structure and parameters 
+URL = 'https://www.football-data.co.uk/mmz4281/2122/E0.csv'
 root = 'https://www.football-data.co.uk/mmz4281/'
 leagues = ['E0', 'E1', 'E2', 'E3', 'EC'] # create a list of leagues: Premier League + lower divisions + Championship
+frames = [] # initialize an empty list to collect intermediate results from the loop
 
-# initialise an empty list to collect intermediate results from the loop
+# ------------------------------------------------------------------------------------
+# one season
+# # loop through leagues, read multiple .csv, append into a list
+# for league in leagues:
+#     df = pd.read_csv(root + '2122' + '/' + league + '.csv', encoding='unicode_escape')
+#     frames.append(df)
+
+# # check the length of frames list. Expected result: 5 (1 season x 5 leagues)
+# # len(frames)
+# ------------------------------------------------------------------------------------
+
+# multiple seasons
 frames = []
 
-# loop through leagues, read multiple .csv, append into a list
+# loop through leagues, seasons, read multiple .csv, append into a list
 for league in leagues:
-    df = pd.read_csv(root + '2122' + '/' + league + '.csv', encoding='unicode_escape')
-    frames.append(df)
+    for season in range(15, 21):
+        df = pd.read_csv(root + (str(season) + str(season+1)) + '/' + league + '.csv', encoding='unicode_escape')
+        df.insert(1, 'season', season)
+        frames.append(df)
 
-# check the length of frames list. Expected result: 5 (5 files in total)
-# len(frames)
+# check the length of frames list. Expected result: 30 (6 seasons x 5 leagues)
+len(frames)
+# show the firs element
+frames[0]
+
