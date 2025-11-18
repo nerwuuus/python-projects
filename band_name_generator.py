1a8aa8fcea37f1d202495c7035d2044bda713934
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2025-11-18 11:29:06 +0100

    Delete band_name_generator.py

--- band_name_generator.py
+++
@@ -1,9 +0,0 @@
-# welcome user prompt
-print("Welcome to the Band Name Generator!")
-
-# user input
-city = input("What's the name of the city you grew up in?")
-pet = input("What's your pet name?")
-
-# print out the result
-print("Your band name could be " + city + " " + pet)
