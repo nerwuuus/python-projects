88a4178ea008788d895c0fa44cf6fc1a6fecf7ed
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2025-12-21 17:08:19 +0100

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -10,3 +10,8 @@ bill_with_tip = bill + (bill * tip / 100)
 bill_split_per_person = round(bill_with_tip / people, 2)
 print(f"Everybody should pay ${bill_split_per_person}.")
 
+# simpler way to calculate the bill split
+tip_percentage = (tip / 100) + 1
+bill_split = round((bill / people) * tip_percentage, 2)
+
+print(f"Each person should pay {bill_split}.")
