38c4549da62dde617c9736098139bc27657e5afc
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2025-12-30 14:15:39 +0100

    Update caesar_cipher_encryption.py

--- caesar_cipher_encryption.py
+++ caesar_cipher_encryption.py
@@ -5,6 +5,11 @@ alphabet = [
   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
+# collect user inputs
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+# define a new function
 def encode(plain_text, shift_amount):
     result = ''
   
@@ -15,4 +20,5 @@ def encode(plain_text, shift_amount):
       
     print(f'Here is your encoded message: {result}')
 
-encode(plain_text='hello', shift_amount=1) # returns 'ifmmp'
+# call a function (in this case it returns 'ifmmp')
+encode(plain_text=text, shift_amount=shift) 
