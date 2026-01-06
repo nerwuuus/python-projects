e497f581e5ce688b40ce7024f79562ccf4bd2e41
Patryk Żychlewicz <63746555+nerwuuus@users.noreply.github.com>
2026-01-06 21:27:37 +0100

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,10 +1,17 @@
-# this function checks if year, e.g., 2000 is leap and returns True or False
+# ============================================================================
+# This is how you work out whether if a particular year is a leap year. 
+# - on every year that is divisible by 4 with no remainder
+# - except every year that is evenly divisible by 100 with no remainder 
+# - unless the year is also divisible by 400 with no remainder   
+# ============================================================================
 def is_leap_year(year):
+    
     if year % 4 == 0 and year % 100 > 0:
         return True
     elif year % 4 > 0:
         return False
     else:
+        
         if year % 400 == 0:
             return True
         else:
